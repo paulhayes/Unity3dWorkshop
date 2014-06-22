c83ae535f1ba6f064be554b1029ef50412f05c96
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-06-22 18:09:37 +0100

    Added new scenes

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour 
+{
+	public bool followX;
+	public bool followY;
+	
+	void Update ()
+	{
+		float depth = Camera.main.transform.InverseTransformPoint( transform.position ).z;
+		Vector3 mouse = new Vector3( Input.mousePosition.x, Input.mousePosition.y, depth );
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint( mouse );
+		if( ! followX ){
+			worldPosition.x = transform.position.x;
+		}
+		if( ! followY ){
+			worldPosition.y = transform.position.y;
+		}
+		
+		if( rigidbody ){
+			rigidbody.position = worldPosition;
+		}
+		else {
+			transform.position = worldPosition;			
+		}
+		
+	}
+}
---
+++ Assets/Scripts/PushOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushOnCollision : MonoBehaviour 
+{
+	public float forceStrength;
+	
+	void Start () 
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+	
+	void OnCollisionEnter(Collision collision){
+		Vector3.
+		Vector3 direction = ( collision.transform.position - transform.position ).normalized;
+		collision.collider.attachedRigidbody.AddForce( forceStrength * direction, ForceMode.Impulse ); 
+	}
+}
