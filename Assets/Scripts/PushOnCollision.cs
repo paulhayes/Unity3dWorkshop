4447827d4105ee060e663b352c2df1cab5e3e153
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-06-24 10:34:26 +0100

    ooops typo

--- Assets/Scripts/PushOnCollision.cs
+++ Assets/Scripts/PushOnCollision.cs
@@ -16,7 +16,6 @@ public class PushOnCollision : MonoBehaviour
 	}
 	
 	void OnCollisionEnter(Collision collision){
-		Vector3.
 		Vector3 direction = ( collision.transform.position - transform.position ).normalized;
 		collision.collider.attachedRigidbody.AddForce( forceStrength * direction, ForceMode.Impulse ); 
 	}
