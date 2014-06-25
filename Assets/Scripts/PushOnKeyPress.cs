36da328be38ce59905d8c4d7d6b8345e72b2759f
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-06-25 18:41:16 +0100

    More simple scripts

---
+++ Assets/Scripts/PushOnKeyPress.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushOnKeyPress : MonoBehaviour 
+{
+	public KeyCode keyCode;
+	public Vector3 force;
+	public Vector3 torque;
+	
+	void Start () 
+	{
+	
+	}
+	
+	void Update ()
+	{
+		if( Input.GetKey( keyCode ) ){
+			
+			rigidbody.AddForce( force, ForceMode.Impulse );
+			rigidbody.AddTorque( torque, ForceMode.Impulse );
+		}
+	}
+}
---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleFollow : MonoBehaviour 
+{
+	public Vector3 distance;
+	public Transform target;
+	
+	void Update ()
+	{
+		transform.position = target.position + distance;
+	}
+}
