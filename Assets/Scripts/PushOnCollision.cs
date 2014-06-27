d94bb61e66941d05fa3ef4684b21d83670553fcd
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-06-28 00:10:12 +0100

    Changed planet paint scene name

--- Assets/Scripts/PushOnCollision.cs
+++ Assets/Scripts/PushOnCollision.cs
@@ -5,16 +5,6 @@ public class PushOnCollision : MonoBehaviour
 {
 	public float forceStrength;
 	
-	void Start () 
-	{
-	
-	}
-	
-	void Update ()
-	{
-	
-	}
-	
 	void OnCollisionEnter(Collision collision){
 		Vector3 direction = ( collision.transform.position - transform.position ).normalized;
 		collision.collider.attachedRigidbody.AddForce( forceStrength * direction, ForceMode.Impulse ); 
