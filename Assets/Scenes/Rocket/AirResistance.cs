97fb5b2f1effcd0319f78543a5e3bf0f5057c046
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-08-25 08:51:41 +0100

    Added Rocket scene

---
+++ Assets/Scenes/Rocket/AirResistance.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirResistance : MonoBehaviour {
+
+    public Vector3 resistanceDirection;
+    public float strength;
+    Rigidbody body;
+	void Awake () {
+	    body = GetComponent<Rigidbody>();
+	}   
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var dir  = transform.TransformDirection( resistanceDirection.normalized );
+        var drag = Vector3.Dot( body.velocity, dir );
+
+        body.AddForce( strength*drag*-1f*dir, ForceMode.Force );
+	}
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        var dir  = transform.TransformDirection( resistanceDirection.normalized );
+        if( Application.isPlaying ) {
+            var drag = Vector3.Dot( body.velocity, dir );
+
+            Gizmos.DrawRay( transform.position, strength*drag*-1f*dir );
+
+        } else {
+            Gizmos.DrawRay( transform.position, dir );
+
+        }
+    }
+}
---
+++ Assets/Scenes/Rocket/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+    public ParticleSystem[] particles;
+    public ConstantForce force;
+    public KeyCode key;
+    public Fuel fuel;
+    public bool ignited;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if( Input.GetKeyDown(key) ){
+            ignited = true;
+            foreach(var particle in particles ){
+                particle.Play();
+            }
+
+        }
+
+        if( ignited ){
+            fuel.quantity -= Time.deltaTime;
+        }
+        if( fuel.quantity <= 0 && force  ){
+            foreach(var particle in particles ){
+                particle.Stop();
+            }
+        }
+        force.enabled = (fuel.quantity>0) && (ignited);
+
+	}
+
+
+}
