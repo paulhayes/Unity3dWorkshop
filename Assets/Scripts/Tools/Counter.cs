d1c47484f636d7efb2dee70be9b91ea1ff72661c
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-08-26 12:40:21 +0100

    Added message counter

---
+++ Assets/Scripts/Tools/Counter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Counter : MonoBehaviour {
+    public int counter = 0;
+    public int goalAmount = 1;
+    public UnityEvent OnGoalEvent;
+
+    bool changed = 0;
+
+	void Increment(){
+        counter++;
+        changed = true;
+    }
+
+    void Decremement(){
+        counter--;
+        changed = true;
+    }
+
+    void Update(){
+        if( changed ){
+            if( counter == goalAmount ){
+                OnGoalEvent.Invoke();
+            }
+            changed = false;
+        }
+        
+    }
+}
