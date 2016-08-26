44002fc27782ae9627eee58c52a4b87f89e877a2
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-08-26 09:15:12 +0100

    Added new rigidbody effect scripts

---
+++ Assets/Scripts/CSharp/Begin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Begin : MonoBehaviour {
+
+    public UnityEvent OnBeginEvent;
+
+	void Start () {
+	    OnBeginEvent.Invoke();
+	}
+	
+
+}
---
+++ Assets/Scripts/CSharp/MoveRigidbodyTo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class MoveRigidbodyTo : MonoBehaviour {
+
+    public float speed;
+
+    public UnityEvent OnMoveComplete;
+
+    Rigidbody body;
+    Vector3 destination;
+    bool moving;
+
+
+    public void MoveRigidBodyTo(Transform dest){
+        destination = dest.position;
+        moving = true;
+    }
+
+    void Awake(){
+        
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        if( !moving ){
+            return;
+        }
+        body.MovePosition( Vector3.MoveTowards( body.position, destination, speed * Time.fixedDeltaTime ) );
+        if( body.position == destination ){
+            moving = false;
+            OnMoveComplete.Invoke();
+        }
+       
+    }
+
+}
